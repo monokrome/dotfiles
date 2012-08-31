f952c93115de6e287a5582645aea0472b8377271
Brandon R. Stoner <bstoner@yrsi.com>
2012-08-31 06:35:45 -0600

    Updated iTerm2 configuration to only be manually updated if necessary

--- darwin/workstation/setup.sh
+++ darwin/workstation/setup.sh
@@ -21,7 +21,18 @@ else
 fi
 
 if [[ $operating_system == darwin ]]; then
-  echo '======> Updating iTerm2 configuration...'
-  defaults write com.googlecode.iterm2 PrefsCustomFolder ~/.iterm2
+  current_iterm_configuration_dir=$(defaults read com.googlecode.iterm2 PrefsCustomFolder)
+  current_directory=$(pwd)
+
+  if [[ -f ${current_iterm_configuration_dir} ]]; then
+    cd $current_iterm_configuration_dir
+  fi
+
+  if [[ $(pwd) == $current_iterm_configuration_dir ]]; then
+    echo '======> Updating iTerm2 configuration...'
+    defaults write com.googlecode.iterm2 PrefsCustomFolder ~/.iterm2
+  fi
+
+  cd ${current_directory}
 fi
 
