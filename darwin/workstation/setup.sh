97730c89f401cc8ba6d8e214d7aafc828202f6da
Brandon R. Stoner <bstoner@yrsi.com>
2012-08-31 06:38:41 -0600

    Fixed issue where detection of iTerm2 configuration was not working properly.

--- darwin/workstation/setup.sh
+++ darwin/workstation/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ ! -e ~/.oh-my-zsh ]]; then
   curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
@@ -24,11 +24,11 @@ if [[ $operating_system == darwin ]]; then
   current_iterm_configuration_dir=$(defaults read com.googlecode.iterm2 PrefsCustomFolder)
   current_directory=$(pwd)
 
-  if [[ -f ${current_iterm_configuration_dir} ]]; then
+  if [[ -e ${current_iterm_configuration_dir} ]]; then
     cd $current_iterm_configuration_dir
   fi
 
-  if [[ $(pwd) == $current_iterm_configuration_dir ]]; then
+  if [[ $(pwd) != $current_iterm_configuration_dir ]]; then
     echo '======> Updating iTerm2 configuration...'
     defaults write com.googlecode.iterm2 PrefsCustomFolder ~/.iterm2
   fi
