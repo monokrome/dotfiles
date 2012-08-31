ad9cf59ed0f105b53c4ea18db07643135fd84b83
Brandon R. Stoner <bstoner@yrsi.com>
2012-08-31 04:47:27 -0600

    Readded my configuration rebased on framework

---
+++ darwin/workstation/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [[ ! -e ~/.oh-my-zsh ]]; then
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+fi
+
+add_configuration .screenrc
+add_configuration .gitconfig
+add_configuration .tmux.conf
+add_configuration .vim
+add_configuration .Xdefault
+
