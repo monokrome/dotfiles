cbdeeece317ab92e1d8b782a1b32b7a0ca2ddb2e
Brandon R. Stoner <bstoner@yrsi.com>
2012-08-31 04:20:55 -0600

    Added OH MY ZSH

--- darwin/workstation/setup.sh
+++ darwin/workstation/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [[ ! -e ~/.oh-my-zsh ]]; then
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+fi
+
 add_configuration .screenrc
 add_configuration .gitconfig
 add_configuration .tmux.conf
