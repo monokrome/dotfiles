2e8aa6ef513ed256fec3b9414ca9929dd07a7975
Brandon R. Stoner <monokrome@limpidtech.com>
2012-09-04 09:18:02 -0600

    Added Terminus font

--- linux/workstation/setup.sh
+++ linux/workstation/setup.sh
@@ -9,6 +9,10 @@ add_configuration .xbindkeysrc
 
 add_configuration bin/brightness
 
+add_configuration .fonts/TerminusBold.ttf
+add_configuration .fonts/TerminusMedium.ttf
+add_configuration .fonts/TerminusMediumItalic.ttf
+
 # A little not for the Linux people!
 echo
 info 'This machine is Linux. You may need to logout for some changes to take affect.'
