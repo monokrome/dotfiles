2666bc9980060bf3b59231509bd3aeff01f7a68e
Brandon R. Stoner <monokrome@limpidtech.com>
2012-09-04 10:19:49 -0600

    Modified the share script to checkout develop for powerline, since master is empty.

--- shared/common_configurations.sh
+++ shared/common_configurations.sh
@@ -23,3 +23,11 @@ else
   info 'Current shell is already zsh. Not changing.'
 fi
 
+vim_directory="${repository_root}/lib/.vim"
+vim_bundle_directory="${vim_directory}/bundle"
+
+# Checkout develop in the powerline repository due to a non-existant master.
+cd "${vim_bundle_directory}/vim-powerline/"
+git checkout develop > /dev/null 2> /dev/null
+cd "${repository_root}"
+
