df053bf214f1cd4823a3237f23f33a0af79004e0
Brandon R. Stoner <monokrome@limpidtech.com>
2013-08-14 22:02:20 -0700

    Update Vim installer.

--- shared/common.sh
+++ shared/common.sh
@@ -2,20 +2,22 @@
 
 add_configuration Pictures/Wallpapers/Smokey_by_monokrome.png
 
-vim_path="${HOME}/.vim"
+function installVim() {
+  vim_path="${HOME}/.vim"
 
-if [[ ! -d "${vim_path}" ]]; then
-  info 'Cloning Vim configuration repository'
+  if [[ ! -d "${vim_path}" ]]; then
+    cd "$HOME" &&
+    git clone http://github.com/monokrome/vim-config .vim > /dev/null 2> /dev/null &&
 
-  cd "$HOME" &&
-  git clone http://github.com/monokrome/vim-config .vim > /dev/null 2> /dev/null &&
+    cd "${vim_path}" &&
+    gits populate > /dev/null 2> /dev/null 
+  fi
 
-  cd "${vim_path}" &&
-  gits populate > /dev/null 2> /dev/null 
-fi
+  vim '+silent BundleInstall' '+qa!' > /dev/null 2> /dev/null &&
 
-info 'Installing new Vim bundles'
-vim '+silent BundleInstall' '+qa!' > /dev/null 2> /dev/null 
+  cd "${vim_path}/bundle/YouCompleteMe/" > /dev/null 2> /dev/null &&
+  ./install.sh --clang-completer > /dev/null 2> /dev/null
+}
 
-info 'Rebuilding YouCompleteMe'
-cd "${vim_path}/bundle/YouCompleteMe/" && ./install.sh --clang-completer > /dev/null 2> /dev/null 
+info 'Configuring Vim (in background)'
+installVim &
