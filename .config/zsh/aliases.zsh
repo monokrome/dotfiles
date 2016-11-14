dc696064577eabaab0ef3164668ea1c5c58b18a0
Bailey Stoner <monokrome@monokro.me>
2016-11-14 11:31:17 -0800

    Fix clean command :)

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,8 +1,7 @@
-alias clean='find . -iname \*.pyc -or -iname .DS_Store -or Thumbds.db -exec rm {} \;'
-alias vim=nvim
-alias freeze="pip freeze | grep -v neovim"
+alias clean='find . -iname \*.pyc -or -iname .DS_Store -or -iname Thumbds.db -exec rm {} \;'
 alias dot='git --git-dir="$HOME/.dotfiles" --work-tree="$HOME"'
-alias npm='yarn'
+alias freeze="pip freeze | grep -v neovim"
+alias vim=nvim
 
 
 # Because OSX is WTF
