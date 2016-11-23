311a8c7d140978d560796cba97f95ea3b6d8406b
Bailey Stoner <monokrome@monokro.me>
2016-11-22 16:24:43 -0800

    Fix color codes

--- .config/zsh/plugins/prompt.zsh
+++ .config/zsh/plugins/prompt.zsh
@@ -7,16 +7,16 @@ set_prompt() {
     PS2=' ->   '
     PS1="%n@%M "
 
-    [[ $previous_command_code != 0 ]] && PS1="${fg[red]}${PS1}${reset_color}"
+    [[ $previous_command_code != 0 ]] && PS1="%F{red}${PS1}${reset_color}"
 
     PS1="$PS1%# "
 
     GIT_REF="$(git symbolic-ref HEAD 2> /dev/null | sed -e 's/refs\/heads\///')"
-    [[ $GIT_REF != "" ]] && PS1="$PS1${fg[cyan]}($vcs_info_msg_0_)${reset_color} "
+    [[ $GIT_REF != "" ]] && PS1="$PS1%F{cyan}($vcs_info_msg_0_)${reset_color} "
 
     if [[ $VIRTUAL_ENV != "" ]]; then
         basename=$(basename $VIRTUAL_ENV)
-        PS1="${fg[cyan]}(${basename})${reset_color} $PS1"
+        PS1="%F{cyan}(${basename})${reset_color} $PS1"
     fi
 }
 
