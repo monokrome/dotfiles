d5eb4d3e3daa1f822be1f15919562b303517928d
Bailey Stoner <monokrome@monokro.me>
2016-11-22 16:17:11 -0800

    Fix ZSH not finding cursor.

--- .config/zsh/plugins/prompt.zsh
+++ .config/zsh/plugins/prompt.zsh
@@ -7,16 +7,18 @@ set_prompt() {
     PS2=' ->   '
     PS1="%n@%M "
 
-    if [[ $previous_command_code != 0 ]]; then
-        PS1="${fg[red]}${PS1}${reset_color}"
-    fi
+    [[ $previous_command_code != 0 ]] && PS1="${fg[red]}${PS1}${reset_color}"
 
     PS1="$PS1%# "
 
     GIT_REF="$(git symbolic-ref HEAD 2> /dev/null | sed -e 's/refs\/heads\///')"
     [[ $GIT_REF != "" ]] && PS1="$PS1${fg[cyan]}($vcs_info_msg_0_)${reset_color} "
-    [[ $VIRTUAL_ENV != "" ]] && PS1="${fg[cyan]}($(basename $VIRTUAL_ENV))${reset_color} $PS1"
+
+    if [[ $VIRTUAL_ENV != "" ]]; then
+        basename=$(basename $VIRTUAL_ENV)
+        PS1="${fg[cyan]}(${basename})${reset_color} $PS1"
+    fi
 }
 
-precmd_functions+=(${precmd_functions[@]} "set_prompt")
 
+precmd_functions+=(${precmd_functions[@]} "set_prompt")
