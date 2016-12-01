c50c77a1e25fd0d76e68bec7c6cf3d387be9c557
Bailey Stoner <monokrome@monokro.me>
2016-12-01 01:56:39 -0800

    Added nocorrect script.

---
+++ .config/zsh/plugins/nocorrect.zsh
@@ -0,0 +1,11 @@
+nocorrect_file="${ZSH_CONFIG_PATH}/nocorrect"
+
+
+if [ -f "${nocorrect_file}" ]; then
+    while read -r command; do
+        if [ $command ]; then
+            alias $command="nocorrect $command"
+        fi
+    done < ${nocorrect_file}
+fi
+
