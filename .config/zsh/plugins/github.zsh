5cf5e6dd5e626935f06b5f36376ce091c39c3c52
Bailey Stoner <monokrome@monokro.me>
2016-12-07 19:17:40 -0800

    Add GitHub script.

---
+++ .config/zsh/plugins/github.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+
+[[ -z $PROJECT_ROOT ]] && PROJECT_ROOT=$HOME/Projects/
+
+
+project() {
+    [[ -z $2 ]] && echo 'Usage: clone <organization> <repository>' && exit 1
+
+    organization_directory="${PROJECT_ROOT}${1}"
+    project_directory="${organization_directory}/${2}"
+
+    if [[ ! -d $project_directory ]]; then
+        [[ ! -d $organization_directory ]] && mkdir -p $organization_directory
+
+        cd $organization_directory
+        git clone github.com:$1/$2
+    fi
+
+    cd $project_directory
+}
