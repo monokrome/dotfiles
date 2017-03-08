5e159147d7e4a5c8d863169f0977c2b7d66d36cd
Bailey Stoner <monokrome@monokro.me>
2017-03-08 15:13:53 -0800

    Add more completion stuffs.

--- .config/zsh/plugins/completion.zsh
+++ .config/zsh/plugins/completion.zsh
@@ -1,7 +1,24 @@
 #!/usr/bin/env zsh
 
 
-autoload -U compinit && compinit -D
+autoload -U compinit
+autoload -U colors
+
+
+# Initialize completion system
+compinit -DC
+
+
+# Breakdown of style completion fields, for reference:
+# :completion - Literal string, always the same.
+# :function   - Name of a specific widget function being used.
+# :completer  - The specific completer being used.
+# :command    - Command name (or sometimes a special string for the completer)
+# :argument   - Which command-line argument is being completed
+# :tag        - The tag which we are currently completing
+#
+# Example matching "..." in `dvips -o ...`: 
+# :completion::complete:dvips:option-o-1:files
 
 
 # Do case-insensitive matching, but prefer exact matches
@@ -11,12 +28,15 @@ zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 
 # Use menu selection for completions
 zstyle ':completion:*' menu select
 
-# Don't ask me if I'm sure that I want completions
+
+# If I hit tab I want them. Don't ask "Are you sure?"
 zstyle ':completion:*' list-prompt ''
 
+
 # Formatting and message
-zstyle ':completion:*' verbose yes
-zstyle ':completion:*:descriptions' format "${fg[yellow]}%B--- %d%b"
-zstyle ':completion:*:warnings' format "${fg[red]}No matches found.$reset_color"
-zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
-zstyle ':completion:*' group-name ''
+zstyle ':completion:*'              completer    _expand _complete _correct _approximate
+zstyle ':completion:*'              group-name   ''
+zstyle ':completion:*'              verbose      yes
+zstyle ':completion:*:corrections'  format '     %B%d (errors: %e)%b'
+zstyle ':completion:*:descriptions' format "     %F{yellow}%B--- %d%b%f"
+zstyle ':completion:*:warnings'     format "     %F{yellow}--- No matches found.%f"
