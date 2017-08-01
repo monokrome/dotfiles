16b2c5b9a187831bf238b4b073feadb24669e61a
Bailey Stoner <bailey@patreon.com>
2017-08-01 14:01:29 -0700

    Let's not worry about nvim for now.

--- .config/zsh/environment.zsh
+++ .config/zsh/environment.zsh
@@ -1,4 +1,4 @@
-export EDITOR=nvim
+export EDITOR=vim
 export GOPATH=~/.config/go
 export HOMEBREW_GITHUB_API_TOKEN=$GITHUB_API_TOKEN
 export ELECTRON_DEBUG=1
@@ -11,4 +11,5 @@ SAVEHIST=0
 HISTSIZE=10000
 
 
-path=($GOPATH/bin .bin/ ./node_modules/.bin $HOME/bin /usr/local/bin/ /Users/monokrome/.local/bin $path)
+path=($GOPATH/bin .bin ./node_modules/.bin $HOME/bin /usr/local/bin /Users/monokrome/.local/bin $path)
+export WEBDEV_ECHO_COMMANDS=1
