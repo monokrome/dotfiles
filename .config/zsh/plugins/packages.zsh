ccf780d2c2bc42f4a082cd81a05927d61ecf72e0
Bailey Stoner <monokrome@monokro.me>
2017-08-12 14:47:40 -0700

    Add aliases for package management.

---
+++ .config/zsh/plugins/packages.zsh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+
+setup_xbps_packaging() {
+    alias pkg="sudo xbps-install -S"
+    alias pkgrm="sudo xbps-remove -Su"
+    alias pkgup="sudo xbps-install -Su"
+}
+
+
+setup_apt_packaging() {
+    alias pkg="sudo apt-get install -y"
+    alias pkgrm="sudo apt-get uninstall --purge"
+    alias pkgup="sudo apt-get update -yy && sudo apt-get upgrade -yy"
+}
+
+
+setup_homebrew_packaging() {
+    alias pkg="brew install"
+    alias pkgrm="brew uninstall"
+    alias pkgup="brew update"
+}
+
+
+setup_packages() {
+    which xbps-install > /dev/null 2> /dev/null && setup_xbps_packaging && return
+    which apt-get > /dev/null 2> /dev/null && setup_apt_packaging && return
+    which brew > /dev/null 2> /dev/null && setup_homebrew_packaging && return
+}
+
+
+setup_packages
