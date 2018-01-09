11a76cdfa73d011c078768b161d1f4846e36e2b5
Bailey Stoner <monokrome@monokro.me>
2018-01-09 05:04:07 -0800

    Add a script for managing n

---
+++ .config/zsh/plugins/n.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+[[ -z $N_PREFIX ]] && export N_PREFIX=${HOME}/.config/.n
+[[ ! -e $N_PREFIX ]] && mkdir -p $N_PREFIX
+
+export path=("${N_PREFIX}/bin" $path)
+which n > /dev/null 2> /dev/null && n latest
