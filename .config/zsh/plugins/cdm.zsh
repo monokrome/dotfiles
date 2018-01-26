5b1399f6451a5df6c7b4e9a67a616ca602366c7f
Bailey Stoner <monokrome@monokro.me>
2018-01-25 21:01:11 -0800

    Modify CDM to run on TTY1.

--- .config/zsh/plugins/cdm.zsh
+++ .config/zsh/plugins/cdm.zsh
@@ -1,10 +1,9 @@
 #!/usr/bin/env zsh
 
 # Show CDM if performing a text-mode login on TTY1
-if [[ "$(tty)" == '/dev/tty2' && -f "$HOME/.cdmrc" ]]; then
+if [[ "$(tty)" == '/dev/tty1' && -f "$HOME/.cdmrc" ]]; then
     cdm=which cdm 2> /dev/null || return
 
     [[ -n "$CDM_SPAWN" ]] && return
     [[ -z "$DISPLAY$SSH_TTY$(pgrep xinit)" ]] && exec $cdm
 fi
-
