129ec30401036d33d8ecdf2fd5710430370234ee
Bailey Stoner <monokrome@monokro.me>
2018-01-29 00:47:08 -0800

    Add touchbad scripts

--- .config/wm/dwm/dwm.sh
+++ .config/wm/dwm/dwm.sh
@@ -5,12 +5,14 @@ sleep 3
 xbindkeys &
 onboard -a &
 start-pulseaudio-x11 &
-rotate_backgrounds &
+user-wallpapers &
+user-screen-rotation &
 compose &
 touchegg &
 cbatticon -n &
 volumeicon &
 dwmstatus &
 statnot &
+user-touchpad &
 
 dwm
