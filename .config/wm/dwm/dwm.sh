76a76d3b9639eb7eefef0a8f8e9dc32ee8035705
Bailey Stoner <monokrome@monokro.me>
2018-02-06 02:27:31 -0800

    Small fixes.

--- .config/wm/dwm/dwm.sh
+++ .config/wm/dwm/dwm.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 
-sleep 3
-
-xbindkeys &
-onboard -a &
-start-pulseaudio-x11 &
 user-wallpapers &
 user-screen-rotation &
 user-compositor &
-touchegg &
-cbatticon -n &
-volumeicon &
-user-statusline &
-user-notifications &
 user-touchpad &
+user-notifications &
+user-statusline &
+xrdb .Xresources &
+start-pulseaudio-x11 &
+xbindkeys &
+touchegg &
+onboard -a &
+cbatticon -n &
+blueman-applet &
+volumeicon &
 
 dwm
