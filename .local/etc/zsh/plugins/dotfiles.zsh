08fda2c195254b7ec524129671f0e6b2a8d433e1
Bailey Stoner <monokrome@monokro.me>
2018-02-27 16:19:58 -0800

    Slight improvement to completion for dot command.
    TODO: Need to figure out how to let completion know where the git
    directory is.

--- .local/etc/zsh/plugins/dotfiles.zsh
+++ .local/etc/zsh/plugins/dotfiles.zsh
@@ -1,2 +1,9 @@
+# Load completion plugin
+autoload -U compinit
+
+# Create a git alias called `dot` for managing dotfiles
 alias dot='git --git-dir="$HOME/.dotfiles" --work-tree="$HOME"'
+
+# Use git autocompletion with the dot alias
+compdef _git dot
 
