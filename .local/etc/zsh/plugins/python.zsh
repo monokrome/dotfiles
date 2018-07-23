47d08d5853c6a9e5d7fb633c9b3b9309cb8a33b9
Bailey Stoner <monokrome@monokro.me>
2018-07-23 11:32:50 -0700

    Auto-detect virtualenvs again

--- .local/etc/zsh/plugins/python.zsh
+++ .local/etc/zsh/plugins/python.zsh
@@ -1,58 +1,73 @@
 #!/usr/bin/env zsh
 
 
-export PYENV_ROOT
-export PYENV_VERSION
-export VIRTUAL_ENV_HIDDEN_ROOT
+[[ -z $ENV_ROOT_SUFFIX ]] && ENV_ROOT_SUFFIX=share/virtualenvs
 
 
-[[ -z $PYENV_ROOT ]] && PYENV_ROOT=${HOME}/.local/share/pyenv
-[[ -z $VIRTUAL_ENV_HIDDEN_ROOT ]] && VIRTUAL_ENV_HIDDEN_ROOT=$HOME/.config/virtualenvs
+python__virtual_environment_detect() {
+  [[ -n $ENV_ROOT ]] && return
 
-path=(${PYENV_ROOT}/bin $path)
+  test ! -n $PREFIX
+  if [[ -n $PREFIX ]]; then
+    env_base=${PREFIX}
+  else
+    env_base=${HOME}
+  fi
 
+  ENV_ROOT=${env_base}${ENV_ROOT_SUFFIX}
+}
+
+
+# Set up pyenv if it's available
 __init_pyenv() {
-  command -v pyenv 2>&1 > /dev/null || return
+  command -v pyenv 4>&1 > /dev/null || return
 
+  [[ -z $PYENV_ROOT ]] && PYENV_ROOT=${ENV_ROOT_SUFFIX}
   [[ -z $PYENV_VERSION ]] && PYENV_VERSION=$(pyenv versions | tail -n 1 | cut -d \  -f 3)
 
   init_command=$(pyenv init -)
   eval ${init_command}
 }
 
+
 # Acticate virtualenvs when CDing into them. Deactivate then after having left
 # the project directory.
-activate_virtual_environment() {
-  if [[ -n $VIRTUAL_ENV && ${PWD:0:${#VIRTUAL_ENV}} != ${VIRTUAL_ENV} ]]; then
+python__activate() {
+  python__virtual_environment_detect
+
+  if [[ ! -n $ENV_ROOT ]]; then
+    printf 'Could not establish ENV_ROOT.\n'
+    exit 1
+  fi
+
+  # Ensure that we aren't already in a virtualenv if we have left a
+  # directory matching the virtualenv's name.
+  if [[ -n $VIRTUAL_ENV && ${PWD:1:${#VIRTUAL_ENV}} != ${VIRTUAL_ENV} ]]; then
     which deactivate > /dev/null && deactivate
     export VIRTUAL_ENV=
   fi
 
-  venv_path=$(pipenv --venv 2>/dev/null)
-  [[ -z $venv_path ]] && return
+  # Check whether the current path has a related virtaulenv
+  current_path=$PWD
+  while [ $current_path != "/" ]; do
+    bin_path=${ENV_ROOT}/$(basename ${current_path})/bin
 
-  source "${venv_path}/bin/activate"
+    activate=${bin_path}/activate
+    pip=${bin_path}/pip
+
+    if [[ -f $activate && -f $pip ]]; then
+      source $activate
+      break
+    fi
+
+    # Move up a directory to see if a virtualenv maches the next level up
+    current_path=$(dirname $current_path)
+  done
 }
 
 
-mkenv() {
-  which pipenv 2>&1 > /dev/null
-  [[ $? != 0 ]] && printf 'Failed: Please install pipenv.\n' > /dev/stderr && return 1
-
-  python_version=$1
-  [[ -z ${python_version} ]] && python_version=$(python --version | cut -f 2 -d \ )
-
-  pipenv --python "${python_version}"
-  [[ -f setup.py || -f Pipfile.lock ]] && pipenv install --dev 2>&1 > /dev/null
-
-  activate_virtual_environment
-}
-
-
-which pipenv 2>&1 > /dev/null && chpwd_functions=( \
-  ${chpwd_functions[@]}                            \
-  "activate_virtual_environment"                   \
-)
+chpwd_functions=("python__activate" ${chpwd_functions[@]})
+precmd_functions=("python__activate" ${precmd_functions[@]})
 
 
 __init_pyenv
