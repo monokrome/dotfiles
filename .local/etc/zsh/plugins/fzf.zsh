0297bd9d520ea1ce982505d646e53d5c5fecb847
Bailey Stoner <monokrome@monokro.me>
2018-07-27 10:53:31 -0700

    Update fzf default command to not use .gitignore - which is just annoying.

--- .local/etc/zsh/plugins/fzf.zsh
+++ .local/etc/zsh/plugins/fzf.zsh
@@ -1,5 +1,7 @@
 alias fzf='fzf --height 60% --reverse'
 
-export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_DEFAULT_COMMAND='ag --hidden --follow -U --ignore-dir .git -g "" -H '
+export FZF_CTRL_T_COMMAND=${FZF_DEFAULT_COMMAND}
 
 [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
+
