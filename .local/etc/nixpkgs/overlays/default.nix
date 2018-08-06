42b0c879472703ce5ba0a8856bcb5e66b4996c19
Bailey Stoner <monokrome@monokro.me>
2018-08-05 17:18:17 -0700

    Add aws-iam-authenticator.

--- .local/etc/nixpkgs/overlays/default.nix
+++ .local/etc/nixpkgs/overlays/default.nix
@@ -6,4 +6,17 @@ self: super: {
       sha256 = "179j94hq1q5v9763mh7hfl652xhaqfnrv032g1asb0c27j6vykhx";
     };
   });
+
+  iam-aws-authenticator = super.buildGoPackage (rec {
+    name = "iam-aws-authenticator";
+
+    goPackagePath = "github.com/kubernetes-sigs/aws-iam-authenticator";
+
+    src = super.fetchFromGitHub {
+      owner = "kubernetes-sigs";
+      rev = "01dd27d77ec1e2ec640a010970f00b2f8074b0b5";
+      repo = "aws-iam-authenticator";
+      sha256 = "1203mqvjya3fm8lfhsgc200bb017qcm9g7z26n826jdhpv516y4q";
+    };
+  });
 }
