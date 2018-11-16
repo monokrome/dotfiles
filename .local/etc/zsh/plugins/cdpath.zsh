b9b5d736a7e6395df03d3415457a9c2a4456e799
Bailey Stoner <monokrome@monokro.me>
2018-11-16 13:40:26 -0800

    Update cdpath to use silent globbing

--- .local/etc/zsh/plugins/cdpath.zsh
+++ .local/etc/zsh/plugins/cdpath.zsh
@@ -1,9 +1,4 @@
 #!/usr/bin/env zsh
 
-cdpaths=(${HOME}/Projects/* ${HOME}/Projects/*/*); for p in $cdpaths; do
-  if [[ -z $CDPATH ]]; then
-    CDPATH="$p"
-  else
-    CDPATH="${CDPATH}:$p"
-  fi
-done && unset cdpaths
+
+cdpath=( $HOME/Projects/{*,*/*}(/N) )
