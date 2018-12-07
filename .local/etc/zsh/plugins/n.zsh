1db3782e8521b632e66f9489cfdfb01412a170c1
Bailey Stoner <monokrome@monokro.me>
2018-12-07 13:45:37 -0800

    Add n to path... oops.

--- .local/etc/zsh/plugins/n.zsh
+++ .local/etc/zsh/plugins/n.zsh
@@ -3,7 +3,13 @@
 export N_PREFIX=${HOME}/.local/share/n
 [[ -z NODE_VERSION ]] && export NODE_VERSION=lts
 
+[[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+
+n_initialize() {
+    n ${NODE_VERSION} 2>&1 > /dev/null
+}
+
 # n is very slow, so we do this async in a subshell
-( which n 2>&1 > /dev/null && n ${NODE_VERSION} 2>&1 > /dev/null & )
+( which n 2>&1 > /dev/null && n_initialize & )
 
 alias npm="PREFIX=${HOME}/.local npm"
